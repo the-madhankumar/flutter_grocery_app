67c6d5624fe77b8e50a1cde4c808172654bfacd2
Madhan Kumar <madhankumarbusiness@gmail.com>
2024-03-27 23:24:10 +0530

    completed

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:groceryapp/model/cart_model.dart';
+import 'package:groceryapp/pages/intro_page.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => CartModel(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: IntroScreen(),
+      ),
+    );
+  }
+}
